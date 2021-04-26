257dfa9b22da4952fd4b3b13841b22cb9400099c
NathanIsBored13 <54941652+NathanIsBored13@users.noreply.github.com>
2021-04-26 17:27:04 +0100

    optimised Logger methods

--- Sandbox/src/MainWindow.cpp
+++ Sandbox/src/MainWindow.cpp
@@ -3,20 +3,7 @@
 
 MainWindow::MainWindow(Oculum::WindowManager* windowManager) : Oculum::Window(L"MainWindow", 400, 400, nullptr, windowManager)
 {
-	Oculum::Layer* layer;
-	layer = new Oculum::Layer("1");
-	GetStack()->PushLayer(layer);
-	layer = new Oculum::Layer("2");
-	GetStack()->PushLayer(layer);
-	layer = new Oculum::Layer("3");
-	GetStack()->PushLayer(layer);
-	new TestEntity(this, 0);
-	new TestEntity(this, 0);
-	new TestEntity(this, 0);
-	new TestEntity(this, 1);
-	new TestEntity(this, 1);
-	new TestEntity(this, 2);
-	OC_INFO(GetStack()->ToString().c_str());
+
 }
 
 MainWindow::~MainWindow()
